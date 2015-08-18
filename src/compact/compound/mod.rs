7d39ed2289aed6e0c4fdcb86a618b29f7f9fd59b
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-08-17 20:34:24 -0400

    Move a macro

--- src/compact/compound/mod.rs
+++ src/compact/compound/mod.rs
@@ -53,6 +53,24 @@ macro_rules! index {
 
 macro_rules! itemize(($code:item) => ($code));
 
+macro_rules! lookup {
+    ($what:ident, $from:ty => $into:ty { $($key:expr => $value:expr,)+ }) => ({
+        use std::collections::HashMap;
+        use std::sync::{ONCE_INIT, Once};
+
+        unsafe {
+            static mut MAP: *const HashMap<$from, $into> = 0 as *const _;
+            static ONCE: Once = ONCE_INIT;
+            ONCE.call_once(|| {
+                let mut map: HashMap<$from, $into> = HashMap::new();
+                $(map.insert($key, $value);)+
+                MAP = ::std::mem::transmute(Box::new(map));
+            });
+            (&*MAP).get(&$what).map(|&value| value)
+        }
+    });
+}
+
 mod dictionary_index;
 mod encoding;
 mod header;
