2335e222a3e4395bcbd256a51ba5b5acc2b3cc9c
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-08-26 07:49:25 -0400

    A cosmetic adjustment

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,7 @@ pub type Error = std::io::Error;
 pub type Result<T> = std::result::Result<T, Error>;
 
 macro_rules! raise(
-    ($message:expr) => (
-        return Err(::Error::new(::std::io::ErrorKind::Other, $message));
-    );
+    ($message:expr) => (return Err(::Error::new(::std::io::ErrorKind::Other, $message)));
     ($($argument:tt)+) => (raise!(format!($($argument)+)));
 );
 
