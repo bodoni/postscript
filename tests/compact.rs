1433ba54fa64df2e2eab3ac221a97822281d830c
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-05-15 09:03:56 +0200

    Make a number of cosmetic adjustments

--- tests/compact.rs
+++ tests/compact.rs
@@ -11,81 +11,6 @@ macro_rules! operations(
 );
 
 #[test]
-fn header() {
-    let set = setup();
-    let table = &set.header;
-
-    assert_eq!(table.major, 1);
-    assert_eq!(table.minor, 0);
-    assert_eq!(table.header_size, 4);
-    assert_eq!(table.offset_size, 2);
-}
-
-#[test]
-fn names() {
-    let set = setup();
-    let vector = &set.names;
-
-    assert_eq!(vector.len(), 1);
-    assert_eq!(&vector[0], "SourceSerifPro-Regular");
-}
-
-#[test]
-fn top_dictionaries() {
-    let set = setup();
-    let vector = &set.top_dictionaries;
-
-    assert_eq!(vector.len(), 1);
-    assert_eq!(&*vector[0], &operations!(
-        Version => [709],
-        Notice => [710],
-        Copyright => [711],
-        FullName => [712],
-        FamilyName => [712],
-        Weight => [388],
-        FontBBox => [-178, -335, 1138, 918],
-        Charset => [8340],
-        Charstrings => [8917],
-        Private => [65, 33671],
-    ));
-}
-
-#[test]
-fn strings() {
-    let set = setup();
-    let index = &set.strings;
-
-    assert_eq!(index.len(), 322);
-    assert_eq!(index.get(175).unwrap(), "Aring");
-    assert_eq!(index.get(500).unwrap(), "nine.tosf");
-}
-
-#[test]
-fn global_subroutines() {
-    let set = setup();
-    let index = &set.global_subroutines;
-
-    assert_eq!(index.len(), 181);
-}
-
-#[test]
-fn encodings() {
-    use postscript::compact::Encoding;
-
-    let set = setup();
-    let vector = &set.encodings;
-    let strings = &set.strings;
-
-    assert_eq!(vector.len(), 1);
-    match &vector[0] {
-        encoding @ &Encoding::Standard => {
-            assert_eq!(strings.get(encoding.get(42).unwrap()).unwrap(), "asterisk");
-        },
-        _ => unreachable!(),
-    }
-}
-
-#[test]
 fn charsets() {
     use postscript::compact::Charset;
 
@@ -109,6 +34,60 @@ fn charstrings() {
 }
 
 #[test]
+fn encodings() {
+    use postscript::compact::Encoding;
+
+    let set = setup();
+    let vector = &set.encodings;
+    let strings = &set.strings;
+
+    assert_eq!(vector.len(), 1);
+    match &vector[0] {
+        encoding @ &Encoding::Standard => {
+            assert_eq!(strings.get(encoding.get(42).unwrap()).unwrap(), "asterisk");
+        },
+        _ => unreachable!(),
+    }
+}
+
+#[test]
+fn global_subroutines() {
+    let set = setup();
+    let index = &set.global_subroutines;
+
+    assert_eq!(index.len(), 181);
+}
+
+#[test]
+fn header() {
+    let set = setup();
+    let table = &set.header;
+
+    assert_eq!(table.major, 1);
+    assert_eq!(table.minor, 0);
+    assert_eq!(table.header_size, 4);
+    assert_eq!(table.offset_size, 2);
+}
+
+#[test]
+fn local_subroutines() {
+    let set = setup();
+    let vector = &set.local_subroutines;
+
+    assert_eq!(vector.len(), 1);
+    assert_eq!(vector[0].len(), 180);
+}
+
+#[test]
+fn names() {
+    let set = setup();
+    let vector = &set.names;
+
+    assert_eq!(vector.len(), 1);
+    assert_eq!(&vector[0], "SourceSerifPro-Regular");
+}
+
+#[test]
 fn private_dictionaries() {
     use postscript::compact::{Number, Operator};
 
@@ -121,10 +100,31 @@ fn private_dictionaries() {
 }
 
 #[test]
-fn local_subroutines() {
+fn strings() {
     let set = setup();
-    let vector = &set.local_subroutines;
+    let index = &set.strings;
+
+    assert_eq!(index.len(), 322);
+    assert_eq!(index.get(175).unwrap(), "Aring");
+    assert_eq!(index.get(500).unwrap(), "nine.tosf");
+}
+
+#[test]
+fn top_dictionaries() {
+    let set = setup();
+    let vector = &set.top_dictionaries;
 
     assert_eq!(vector.len(), 1);
-    assert_eq!(vector[0].len(), 180);
+    assert_eq!(&*vector[0], &operations!(
+        Version => [709],
+        Notice => [710],
+        Copyright => [711],
+        FullName => [712],
+        FamilyName => [712],
+        Weight => [388],
+        FontBBox => [-178, -335, 1138, 918],
+        Charset => [8340],
+        Charstrings => [8917],
+        Private => [65, 33671],
+    ));
 }
