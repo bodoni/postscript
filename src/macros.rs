9cf50c011e48cf2b846d1b50e2418e64f40542a4
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-07-30 15:08:14 +0200

    Make a cosmetic adjustment

--- src/macros.rs
+++ src/macros.rs
@@ -56,20 +56,20 @@ macro_rules! read_walue(
 
 macro_rules! table {
     ($(#[$attribute:meta])* pub $structure:ident {
-        $($field:ident ($kind:ty),)+
+        $($field:ident ($kind:ty),)*
     }) => (
-        table! { @define $(#[$attribute])* pub $structure { $($field ($kind),)+ } }
-        table! { @implement pub $structure { $($field,)+ } }
+        table! { @define $(#[$attribute])* pub $structure { $($field ($kind),)* } }
+        table! { @implement pub $structure { $($field,)* } }
     );
     (@define $(#[$attribute:meta])* pub $structure:ident {
-        $($field:ident ($kind:ty),)+
+        $($field:ident ($kind:ty),)*
     }) => (itemize! {
         $(#[$attribute])*
         #[derive(Clone, Debug, Eq, PartialEq)]
-        pub struct $structure { $(pub $field: $kind,)+ }
+        pub struct $structure { $(pub $field: $kind,)* }
     });
     (@implement pub $structure:ident {
-        $($field:ident,)+
+        $($field:ident,)*
     }) => (
         impl ::Value for $structure {
             fn read<T: ::Tape>(tape: &mut T) -> ::Result<Self> {
