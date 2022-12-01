179371c9ff002ed60e9cdc7c84e3a3acfbc36ea7
Ivan Ukhov <ivan.ukhov@gmail.com>
2022-12-01 16:14:26 +0100

    Update and trigger the benchmarks

--- benches/type2.rs
+++ benches/type2.rs
@@ -3,6 +3,7 @@
 extern crate postscript;
 extern crate test;
 
+use postscript::compact1::font_set::Record;
 use postscript::type2::Program;
 use test::Bencher;
 
@@ -14,8 +15,11 @@ use common::setup;
 #[bench]
 fn program(bencher: &mut Bencher) {
     let set = setup();
-    let global = &set.global_subroutines;
-    let local = &set.local_subroutines[0];
+    let global = &set.subroutines;
+    let local = match &set.records[0] {
+        Record::CharacterNameKeyed(ref record) => &*record.subroutines,
+        _ => unreachable!(),
+    };
     bencher.iter(|| {
         for code in set.char_strings[0].iter() {
             let mut program = Program::new(code, global, local);
