0dbc73393d2a7472a54d1b0ed9510e0c98d8e5ed
Ivan Ukhov <ivan.ukhov@gmail.com>
2022-12-04 11:38:17 +0100

    Add a placeholder for Type 1

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 extern crate typeface;
 
 pub mod compact1;
+pub mod type1;
 pub mod type2;
 
 pub use typeface::{Error, Result, Tape, Value, Walue};
---
+++ src/type1/mod.rs
@@ -0,0 +1,3 @@
+//! The [Type 1 font format][1].
+//!
+//! [1]: https://adobe-type-tools.github.io/font-tech-notes/pdfs/T1_SPEC.pdf
