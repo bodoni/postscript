d8b63070051e398237ccb941f4c69c4f8b1a441b
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-02-16 15:32:35 +0100

    Remove unsafe code

--- src/compact1/offset.rs
+++ src/compact1/offset.rs
@@ -11,16 +11,12 @@ impl crate::walue::Read<'static> for Offset {
     type Parameter = OffsetSize;
 
     fn read<T: crate::tape::Read>(tape: &mut T, size: OffsetSize) -> Result<Self> {
-        #[cfg(target_endian = "big")]
-        macro_rules! assemble(($hi:expr, $me:expr, $lo:expr) => ([0, $hi, $me, $lo]));
-        #[cfg(target_endian = "little")]
-        macro_rules! assemble(($hi:expr, $me:expr, $lo:expr) => ([$lo, $me, $hi, 0]));
         Ok(Offset(match size {
             1 => tape.take::<u8>()? as u32,
             2 => tape.take::<u16>()? as u32,
             3 => {
-                let trio: [u8; 3] = tape.take()?;
-                unsafe { std::mem::transmute::<_, u32>(assemble!(trio[0], trio[1], trio[2])) }
+                let value: [u8; 3] = tape.take()?;
+                u32::from_be_bytes([0, value[0], value[1], value[2]])
             }
             4 => tape.take::<u32>()?,
             _ => raise!("found a malformed offset"),
